208025c6b1bf6c4e701e08a50675564719ca29d2
Augustine A Lartey <augustinelartey68@gmail.com>
2024-04-04 21:34:33 +0000

    Update to contollers and routes

---
+++ Backend/controllers/gigsControllers.js
@@ -0,0 +1,47 @@
+const Gig = require('../models/gigModels')
+
+// get all gigs
+const getGigs = async (req, res) => {
+    const gigs = await Gig.find({}).sort({createdAt: -1})
+
+    res.status(200).json(gigs)
+}
+
+// get a single gig
+const getGig = async (req, res) => {
+    const {} = req.params
+
+    const gig = await Gig.findById(id)
+
+    if (!gig) {
+        return res.status(400).json({error: 'No such Gig'})
+    }
+
+    res.status(200).json(gig)
+}
+
+
+// create new gig
+const createGig = async (req, res) => {
+    const {title, description, requirements, budget, employerId, category, deadline, attachments} = req.body
+
+    // add doc to db
+try {
+    const gig = await Gig.create({title, description, requirements, budget, employerId, category, deadline, attachments})
+    res.status(200).json(gig)
+} catch (error) {
+    res.status(400).json({error: error.message})
+}
+}
+
+// delete a gig
+
+
+// update a gig
+
+
+module.export = {
+    getGigs,
+    getGig,
+    createGig
+}
--- Backend/routes/gig.js
+++ Backend/routes/gig.js
@@ -1,28 +1,20 @@
 const express = require('express')
-const Gig = require('../models/gigModels')
+const {
+    getGigs,
+    getGig,
+    createGig,
+} = require('../controllers/gigsControllers')
 
 const router = express.Router()
 
 //GET all gigs
-router.get('/', (req, res) => {
-    res.json({mssg: 'GET all gigs'})
-})
+router.get('/', getGigs)
 
 //GET a single gigs
-router.get('/:id', (req, res) => {
-    res.json({mssg: 'GET a single gig'})
-})
+router.get('/:id', getGig)
 
 //POST a new gigs
-router.post('/', async (req, res) => {
-    const {title, description, requirements, budget, employerId, category, deadline, attachments} = req.body
-try {
-    const gig = await Gig.create({title, description, requirements, budget, employerId, category, deadline, attachments})
-    res.status(200).json(gig)
-} catch (error) {
-    res.status(400).json({error: error.message})
-}
-})
+router.post('/', createGig)
 
 //DELETE a  gigs
 router.delete('/:id', (req, res) => {
